42efe4f4b20b533463743b7e376d16412d490e6b
Kim Panudda <kimpanudda@gmail.com>
2024-08-01 16:25:16 -0700

    Fixed cart page file

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -4,7 +4,7 @@ from pages.base_page import Page
 
 
 class CartPage(Page):
-    CART_EMPTY_MSG = (By.CSS_SELECTOR, "[@data-test='boxEmptyMsg'] h1")
+    CART_EMPTY_MSG = (By.CSS_SELECTOR, "[data-test='boxEmptyMsg'] h1")
 
     def verify_cart_empty(self):
         expected_text = 'Your cart is empty'
